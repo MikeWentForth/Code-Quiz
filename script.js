e5713710c4f0ad09d79182493c082f601fdbd807
MikeWentForth <themikewallace@gmail.com>
2023-07-10 10:26:48 -0700

    updated JS file

--- script.js
+++ script.js
@@ -1,3 +1,168 @@
+// create a quiz class
+
+class quiz {
+  constructor(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+  }
+
+
+  getQuestionIndex() {
+
+    return this.questions[this.questionIndex];
+  }
+
+  guess(answer) {
+    if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+      this.score++;
+    }
+    this.questionIndex++;
+  }
+
+  isEnded() {
+
+    return this.questionIndex === this.questions.length;
+  }
+
+}
+
+//CREATE A QUESTION CLASS
+
+class question {
+  constructor(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+  }
+  isCorrectAnswer(choice) {
+    return this.answer === choice;
+  }
+}
+
+//DISPLAY QUESTION
+
+function displayQuestion() {
+  if (quiz.isEnded()) {
+    showScores();
+  } else {
+    let questionElement = document.getElementById("question");
+    questionElement.innerHTML = quiz.getQuestionIndex().text;
+
+    //SHOW OPTIONS
+
+    let choices = quiz.getQuestionIndex().choices;
+    for (let i = 0; i < choices.length; i++) {
+      let choiceElement = document.getElementById("choice" + i);
+      choiceElement.innerHTML = choices[i];
+      guess("btn" + i, choices[i]);
+
+    }
+
+    showProgress();
+  }
+
+};
+
+//GUESS FUNCTION
+
+function guess(id, guess) {
+
+  let button = document.getElementById(id);
+  button.onclick = function () {
+    quiz.guess(guess);
+    displayQuestion();
+  }
+}
+
+//SHOW QUIZ PROGRESS
+
+function showProgress() {
+
+  let currentQuestionNumber = quiz.questionIndex + 1;
+  let progressElement = document.getElementById("progress");
+  progressElement.innerHTML =
+    `Question ${currentQuestionNumber} of ${quiz.question.length}
+    `;
+
+}
+
+//SHOW SCORE
+
+function showScores() {
+  let quizEndHTML =
+
+    `
+  <h1>Quiz Completed</h1>
+  <h2 id= "score">You Scored: ${quiz.score} of ${quiz.question.length}</h2>
+  <div class= "quiz-repeat">
+    <a href="index.html">Take Quiz Again</a>
+  </div>
+  
+  `;
+
+  let quizElement = document.getElementById("quiz");
+  quizElement.innerHTML = quizEndHTML;
+
+}
+
+//CREATE QUIZ QUESTIONS
+
+let questions = [
+  new question(
+    "This is my first question?", ["ans1", "ans2", "ans3", "ans4"], "ans2"
+  ),
+
+  new question(
+    "This is my second question?", ["ans1", "ans2", "ans3", "ans4"], "ans3"
+  ),
+
+  new question(
+    "This is my third question?", ["ans1", "ans2", "ans3", "ans4"], "ans1"
+  ),
+
+  new question(
+    "This is my fourth question?", ["ans1", "ans2", "ans3", "ans4"], "ans4"
+  )
+];
+
+let quiz = new quiz(questions);
+
+//DISPLAY QUESTION
+
+displayQuestion();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // bring in our HTML ELEMENT references
 // var startBtn = document.querySelector('.timer__btn');
 // var questionTitle = document.getElementById('question-title');
